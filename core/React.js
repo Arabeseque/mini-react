c0bc65455ad49d5d9b715dc892a8f27f9120a728
rio <hzy1210085279@gmail.com>
2024-01-14 20:49:25 +0800

    实现 fiber 架构

--- core/React.js
+++ core/React.js
@@ -1,17 +1,10 @@
 function render(el, container) {
-    const dom = el.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(el.type);
-    Object.keys(el.props).forEach(key => {
-        if (key !== 'children') {
-            dom[key] = el.props[key];
+    initialWork = {
+        dom: container,
+        props: {
+            children: [el]
         }
-    })
-
-    const children = el.props.children || [];
-    children.forEach(child => {
-        render(child, dom);
-    })
-
-    container.append(dom);
+    }
 }
 
 function createTextNode(text) {
@@ -33,7 +26,69 @@ function createElement(type, props, ...children) {
         }
     }
 }
-;
+
+let initialWork = null
+function workLoop(deadline) {
+    let shouldYield = false
+    while (!shouldYield && initialWork) {
+        shouldYield = deadline.timeRemaining() < 1
+        initialWork = performUnitOfWork(initialWork)
+    }
+
+    requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+function performUnitOfWork(work) {
+    // 1.创建DOM
+    if (!work.dom) {
+        const dom = work.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(work.type);
+        work.dom = dom;
+        work.parent.dom.append(dom);
+
+    }
+
+    // 2.给DOM添加属性
+    Object.keys(work.props).forEach(key => {
+        if (key !== 'children') {
+            work.dom[key] = work.props[key];
+        }
+    })
+
+    // 3.转换链表
+    const children = work.props.children || [];
+    let preWork = null
+    children.forEach((child, index) => {
+        const newWork = {
+            dom: null,
+            props: child.props,
+            child: null,
+            sibling: null,
+            parent: work,
+            type: child.type
+        }
+        if (index === 0) {
+            work.child = newWork
+        } else {
+            preWork.sibling = child
+        }
+        preWork = newWork
+    })
+
+    if (work.child) {
+        return work.child
+    }
+
+    if (work.sibling) {
+        return work.sibling
+    }
+
+    return work.parent?.sibling
+
+
+}
+
 const React = {
     createElement,
     createTextNode,
